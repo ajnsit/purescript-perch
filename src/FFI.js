40564ceb7e4f2a323910685246efdc0b91b8e0f0
Anupam | अनुपम <ajnsit@gmail.com>
2022-04-28 20:38:23 +0000

    Update FFI.js

--- src/FFI.js
+++ src/FFI.js
@@ -1,34 +1,118 @@
-exports.js_document = function () { return document; };
-exports.js_documentBody = function() { return document.body; };
-exports.js_getElementById = function(x) { return function() { return document.getElementById(x); }; };
-exports.js_querySelectorAll = function(x) { return function() { return document.querySelectorAll(x); }; };
-exports.js_documentCreateNode = function(x) {return function() { return document.createElement(x); }; };
-exports.js_documentCreateNodeNS = function(x) {return function(y) { return function() { return document.createElementNS(x,y); }; }; };
-exports.js_createTextNode = function(x) {return function() { return document.createTextNode(x); };};
+export function js_document() {
+  return document;
+}
+export function js_documentBody() {
+  return document.body;
+}
+export function js_getElementById(x) {
+  return function () {
+    return document.getElementById(x);
+  };
+}
+export function js_querySelectorAll(x) {
+  return function () {
+    return document.querySelectorAll(x);
+  };
+}
+export function js_documentCreateNode(x) {
+  return function () {
+    return document.createElement(x);
+  };
+}
+export function js_documentCreateNodeNS(x) {
+  return function (y) {
+    return function () {
+      return document.createElementNS(x, y);
+    };
+  };
+}
+export function js_createTextNode(x) {
+  return function () {
+    return document.createTextNode(x);
+  };
+}
 
 // Check if object is an HTML Element of current DOM.  Works with HTML
 // elements and text nodes.
 // http://stackoverflow.com/a/20476546/1749901
-exports.js_isInCurrentDOM = function(x) { return function() {
-    return (x !== null && !(!x.ownerDocument) && (window === (x.ownerDocument.defaultView || x.ownerDocument.parentWindow)));
-}; };
+export function js_isInCurrentDOM(x) {
+  return function () {
+    return x !== null && !!x.ownerDocument && window === (x.ownerDocument.defaultView || x.ownerDocument.parentWindow);
+  };
+}
 
-exports.js_parentNode = function(x) { return function() { return x.parentNode; };};
+export function js_parentNode(x) {
+  return function () {
+    return x.parentNode;
+  };
+}
 
-exports.js_appendChild = function(x) { return function(y) { return function() { x.appendChild(y);}; }; };
+export function js_appendChild(x) {
+  return function (y) {
+    return function () {
+      x.appendChild(y);
+    };
+  };
+}
 
-exports.js_replaceChild = function(x) { return function(y) { return function(z) { return function() { x.replaceChild(y,z);};};};};
+export function js_replaceChild(x) {
+  return function (y) {
+    return function (z) {
+      return function () {
+        x.replaceChild(y, z);
+      };
+    };
+  };
+}
 
-exports.js_removeChild = function(x) { return function(y) { return function() {x.removeChild(y);};};};
+export function js_removeChild(x) {
+  return function (y) {
+    return function () {
+      x.removeChild(y);
+    };
+  };
+}
 
-exports.js_clearChildren = function(x) { return function() {
-  while (x.hasChildNodes()) x.removeChild(x.lastChild);
-};};
+export function js_clearChildren(x) {
+  return function () {
+    while (x.hasChildNodes()) x.removeChild(x.lastChild);
+  };
+}
 
-exports.js_setAttribute = function(x) { return function(y) { return function(z) { return function() {x.setAttribute(y, z); }; };};};
+export function js_setAttribute(x) {
+  return function (y) {
+    return function (z) {
+      return function () {
+        x.setAttribute(y, z);
+      };
+    };
+  };
+}
 
-exports.js_setInnerHtml = function(x) { return function(y) { return function() {x.innerHTML = y;}; };};
+export function js_setInnerHtml(x) {
+  return function (y) {
+    return function () {
+      x.innerHTML = y;
+    };
+  };
+}
 
-exports.js_addEventListener = function(x) { return function(y) { return function(z) { return function() { x.addEventListener(y,z);};};};};
+export function js_addEventListener(x) {
+  return function (y) {
+    return function (z) {
+      return function () {
+        x.addEventListener(y, z);
+      };
+    };
+  };
+}
 
-exports.js_removeEventListener = function(x) { return function(y) { return function(z) { return function() { x.removeEventListener(y,z);};};};};
+export function js_removeEventListener(x) {
+  return function (y) {
+    return function (z) {
+      return function () {
+        x.removeEventListener(y, z);
+      };
+    };
+  };
+}
